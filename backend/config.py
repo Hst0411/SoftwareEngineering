4bb6967e42293f2dd36e3c6ecb6eacfef5f5a7d6
kevinkaitung <dadado0570@gmail.com>
2021-12-27 00:57:46 +0800

    add config file

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+# Define connection string (one only) mongodb://<user>:<password>@<mongodbURL>
+host = "mongodb+srv://seproject:kevin@cluster0.bj6up.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+# Select Database (one only)
+db = "SEProject"
+
+# Define collections (can be multiple)
+memberCol = "member"
+recordCol = "record"
+recordCategoryCol = "recordCategory"
+budgetCol = "budget"
+accountCol = "account"
