f1bc4a895f582a4e58c224905f22627d43c22e60
kevinkaitung <dadado0570@gmail.com>
2022-01-08 18:57:58 +0800

    currency exchange rate API complete
    貨幣轉換匯率API完成

---
+++ backend/currencyExchangeModule.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, Blueprint
+from flask.blueprints import Blueprint
+import requests
+
+appCurrencyExchange = Blueprint('appCurrencyExchange', __name__)
+
+
+@appCurrencyExchange.route("/currencyExchange/get-exchange-rate", methods=["GET"])
+def get_exchange_rate():
+    targetCurrency = request.args.get("targetCurrency")
+    return str(cal_exchange_rate(targetCurrency))
+
+
+def cal_exchange_rate(targetCurrency):
+    r=requests.get("https://openapi.taifex.com.tw/v1/DailyForeignExchangeRates",verify=True)
+    target=r.json()
+    if targetCurrency == "TWD":
+        return 1
+    elif targetCurrency == "EUR":
+        return round(float(target[len(target)-1]['EUR/USD'])*float(target[len(target)-1]['USD/NTD']),2)
+    elif targetCurrency == "JPY":
+        return round(float(target[len(target)-1]['USD/NTD'])/float(target[len(target)-1]['USD/JPY']),2)
+    elif targetCurrency == "RMB":
+        return round(float(target[len(target)-1]['RMB/NTD']),2)
+    elif targetCurrency == "GBP":
+        return round(float(target[len(target)-1]['GBP/USD'])*float(target[len(target)-1]['USD/NTD']),2)
+    elif targetCurrency == "AUD":
+        return round(float(target[len(target)-1]['AUD/USD'])*float(target[len(target)-1]['USD/NTD']),2)
+    elif targetCurrency == "HKD":
+        return round(float(target[len(target)-1]['USD/NTD'])/float(target[len(target)-1]['USD/HKD']),2)
+    elif targetCurrency == "ZAR":
+        return round(float(target[len(target)-1]['USD/NTD'])/float(target[len(target)-1]['USD/ZAR']),2)
+    elif targetCurrency == "NZD":
+        return round(float(target[len(target)-1]['NZD/USD'])*float(target[len(target)-1]['USD/NTD']),2)
+    elif targetCurrency == "USD":
+        return round(float(target[len(target)-1]['USD/NTD']),2)
