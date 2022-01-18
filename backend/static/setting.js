7d9fbb8cb5e066dcdce273fc447cb4a8f416e22c
00857133a <hkgjriotod45889@gmail.com>
2022-01-18 22:00:49 +0800

    csv 前端連接
    csv前端連接

--- backend/static/setting.js
+++ backend/static/setting.js
@@ -17,3 +17,33 @@ function GetCurrencyValue(){
     });
     alert("貨幣轉換成功!");
 }
+
+function json2csv(){
+    $.ajax({
+        url: 'record/get-csv?',
+        method: 'GET',
+        dataType: 'json',
+        success: function(target) {
+            for(var i=0;i<target.length;i++){
+                final+=target[i].accountName+',';
+                if(target[i].budgetName==null){
+                    final+='無,';
+                }
+                else{
+                    final+=target[i].budgetName+',';
+                }
+                final+=target[i].category+',';
+                final+=target[i].incomeOrExpense+',';
+                final+=target[i].moneyAmount+',';
+                final+=target[i].date+',';
+                final+='\n';
+           }
+           let filename='csv檔_'+(new Date()).getTime()+'.csv';
+           let link = document.createElement('a');
+            link.setAttribute('href', 'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURI(final));
+            link.setAttribute('download', filename);
+            link.click();
+        }
+    });
+   
+}
